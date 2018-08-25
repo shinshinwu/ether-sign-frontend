444e88ec3d1f5dc999811cee0c876e9bf692eefa
Anna Wu <shinshinwu@gmail.com>
2018-08-25 22:24:14 +0300

    added favicon

--- src/util/pollWeb3.js
+++ src/util/pollWeb3.js
@@ -2,7 +2,7 @@ let pollWeb3 = function (state) {
   setInterval(() => {
     if (state.web3.loaded && state.web3.isInjected) {
       state.web3.web3Instance().eth.getCoinbase((err, coinbase) => {
-        if (coinbase.toLowerCase() !== state.web3.coinbase.toLowerCase()) {
+        if (coinbase && (coinbase.toLowerCase() !== state.web3.coinbase.toLowerCase())) {
           location.reload();
         }
       })
